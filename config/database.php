42110cbe5686aff4876459e66c02b327b458a72a
Felipe Santos de Lima <fslima@ymail.com>
2017-07-16 19:26:21 -0300

    Change config database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 return [
 
     /*
@@ -41,11 +43,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url["host"]),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr($url["path"], 1)),
+            'username' => env('DB_USERNAME', $url["user"]),
+            'password' => env('DB_PASSWORD', $url["pass"]),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

1c9dc417267d6ee7f6e73bb47381084c804529fd
Felipe Santos de Lima <fslima@ymail.com>
2017-07-16 21:24:50 -0300

    change seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(TimesTableSeeder::class);
-        $this->call(CampeonatosTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
+//        $this->call(TimesTableSeeder::class);
+//        $this->call(CampeonatosTableSeeder::class);
     }
 }

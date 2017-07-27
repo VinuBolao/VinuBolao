51945235457164cf15f7f0f4d8488ed864301c13
GUSTAVO <gustavo.santos@atitude.com.br>
2017-07-27 11:45:15 -0300

    Alterei o UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,72 +17,84 @@ class UsersTableSeeder extends Seeder
                 "id" => 2,
                 "name" => "Gustavo Santos",
                 "email" => "gtvslima@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 1
             ],
             [
                 "id" => 22,
                 "name" => "Victor Hugo",
                 "email" => "victorhugo.jsantos@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 1
             ],
             [
                 "id" => 32,
                 "name" => "Marcelo de Melo Lima",
                 "email" => "marceloml1804@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 42,
                 "name" => "Felipe Lima",
                 "email" => "fslima06@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 1
             ],
             [
                 "id" => 52,
                 "name" => "Douglas Santos",
                 "email" => "douglasf.s791@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 62,
                 "name" => "Roberto Melo Lima",
                 "email" => "roberto.melolima@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 72,
                 "name" => "Milton de Melo Lima",
                 "email" => "miltao601957@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 82,
                 "name" => "Denis Santos de Lima",
                 "email" => "denis1703@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 92,
                 "name" => "Thiago Moreira",
                 "email" => "thiago.sanvel10@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 102,
                 "name" => "Moacir de Melo Lima",
                 "email" => "mlmoacir1910@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 112,
                 "name" => "Leandro Reis",
                 "email" => "leandro0318@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ],
             [
                 "id" => 122,
                 "name" => "Fabrício Lima",
                 "email" => "fabriciodelima@gmail.com",
+                'password' => bcrypt('123456'),
                 "master" => 0
             ]
         ];

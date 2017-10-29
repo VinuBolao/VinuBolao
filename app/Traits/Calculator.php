0b3c232e7c0c3352e51af335943b8dbab4cc8432
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2017-10-29 17:32:06 -0200

    Alteração na Trait

--- app/Traits/Calculator.php
+++ app/Traits/Calculator.php
@@ -14,20 +14,16 @@ trait Calculator
     {
         $dados = (object)['pontosganhos' => 0, 'placarexato' => 0, 'placarvencedor' => 0];
 
-        if($placar1 === null || $placar2 === null || $palpite1 === null || $palpite2) {
+        if($placar1 === $palpite1 && $placar2 === $palpite2){
+            $dados->pontosganhos = 10;
+            $dados->placarexato++;
+            return $dados;
+        } elseif (($placar1 === $placar2 && $palpite1 === $palpite2) || ($placar1 > $placar2 && $palpite1 > $palpite2) || ($placar1 < $placar2 && $palpite1 < $palpite2)) {
+            $dados->pontosganhos = 7;
+            $dados->placarvencedor++;
             return $dados;
         } else {
-            if($placar1 === $palpite1 && $placar2 === $palpite2) {
-                $dados->pontosganhos = 10;
-                $dados->placarexato++;
-                return $dados;
-            } elseif (($placar1 === $placar2 && $palpite1 === $palpite2) || ($placar1 > $placar2 && $palpite1 > $palpite2) || ($placar1 < $placar2 && $palpite1 < $palpite2)) {
-                $dados->pontosganhos = 7;
-                $dados->placarvencedor++;
-                return $dados;
-            } else {
-                return $dados;
-            }
+            return $dados;
         }
     }
 }

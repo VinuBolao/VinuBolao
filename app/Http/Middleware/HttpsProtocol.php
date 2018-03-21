3f52b3269cc0b5869db91cd023f915e5414acaab
Gustavo Santos <gustavo.santos@atitude.com.br>
2018-03-21 10:33:08 -0300

    Ajuste para https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,8 +8,10 @@ class HttpsProtocol
 {
     public function handle($request, Closure $next)
     {
-        if (env('APP_ENV') === 'production') {
-            return redirect()->secure($request->getRequestUri());
+        if (!$request->secure()) {
+            if (env('APP_ENV') === 'production') {
+                return redirect()->secure($request->getRequestUri());
+            }
         }
 
         return $next($request);

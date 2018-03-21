cb93a014dc8e4401a2edf9570512f1d3e51372c2
Gustavo Santos <gustavo.santos@atitude.com.br>
2018-03-21 14:40:59 -0300

    Ajuste HTTPS

---
+++ app/Http/Middleware/ForceHttpProtocol.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bolao\Http\Middleware;
+
+use Closure;
+
+class ForceHttpProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}

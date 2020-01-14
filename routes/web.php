e7cb3880cba1a6874cea50380f758c4328f538da
atitude-gustavosantos <gustavo.santos@atitude.com.br>
2020-01-14 11:08:26 -0300

    Ajuste no HTTPS

--- routes/web.php
+++ routes/web.php
@@ -15,36 +15,34 @@ Route::redirect('/', '/bolao')->secure();
 
 Auth::routes();
 
-Route::group(['middleware' => ['forceSSL']], function() {
-	Route::namespace('Admin\\')->group(function () {
-		Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-			Route::resource('user', 'UserController');
-		});
-		
-		Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-			Route::resource('campeonato', 'CampeonatoController');
-		});
-		
-		Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-			Route::resource('time', 'TimeController');
-		});
-		
-		Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-			Route::resource('jogo', 'JogoController');
-		});
+Route::namespace('Admin\\')->group(function () {
+	Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+		Route::resource('user', 'UserController');
 	});
 	
-	Route::middleware(['auth'])->group(function () {
-		Route::get('classificacao', 'BolaoController@classificacao')->name('classificacao');
-		
-		Route::get('/regulamento', 'BolaoController@regulamento')->name('regulamento');
-		
-		Route::resource('bolao', 'BolaoController');
-		
-		Route::get('participante', 'ParticipanteController@index')->name('participante');
-		
-		Route::get('jogo', 'JogoController@index')->name('jogo.index');
-		
-		Route::get('palpite', 'PalpiteController@index')->name('palpite.index');
+	Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+		Route::resource('campeonato', 'CampeonatoController');
 	});
+	
+	Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+		Route::resource('time', 'TimeController');
+	});
+	
+	Route::group(['middleware' => ['auth', 'can:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+		Route::resource('jogo', 'JogoController');
+	});
+});
+
+Route::middleware(['auth'])->group(function () {
+	Route::get('classificacao', 'BolaoController@classificacao')->name('classificacao');
+	
+	Route::get('/regulamento', 'BolaoController@regulamento')->name('regulamento');
+	
+	Route::resource('bolao', 'BolaoController');
+	
+	Route::get('participante', 'ParticipanteController@index')->name('participante');
+	
+	Route::get('jogo', 'JogoController@index')->name('jogo.index');
+	
+	Route::get('palpite', 'PalpiteController@index')->name('palpite.index');
 });

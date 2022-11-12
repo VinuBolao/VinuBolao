406a7923b661eb9a4ac291afc16cc98658beb3de
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2022-11-12 13:46:55 -0300

    Ajustes no Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,13 +21,14 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D0R248F7NH"></script>
         <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-106977927-1', 'auto');
-            ga('send', 'pageview');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D0R248F7NH');
         </script>
 
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])

f5c1f79d10f3c1943f34aa94b99facfde37e3ca0
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2023-04-19 12:01:34 -0300

    Ajuste nas configurações do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/js/Pages/*.{html,js,jsx}",
+        "./resources/js/Components/*.{html,js,jsx}",
     ],
     theme: {
         extend: {

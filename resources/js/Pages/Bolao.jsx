29e07532047cf34c0262dbcced0979ca3425b42a
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2023-04-19 11:42:11 -0300

    Novo Componente
    Criando o componente de modal para ser reutilizado.

--- resources/js/Pages/Bolao.jsx
+++ resources/js/Pages/Bolao.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { Inertia } from "@inertiajs/inertia";
-import { EditIcon, FinishIcon, ParticipantsIcon, QuestionIcon } from "../helpers";
+import ModalDelete from "../Components/ModalDelete";
+import { EditIcon, FinishIcon, ParticipantsIcon } from "../helpers";
 
 const Bolao = ({ bolaos, user }) => {
     const [modal, setModal] = useState(false);
@@ -96,38 +97,7 @@ const Bolao = ({ bolaos, user }) => {
                         </div>
                     ))}
 
-                    {modal && (
-                        <div className="fixed flex items-center justify-center top-0 left-0 overflow-x-hidden overflow-y-auto bg-black bg-opacity-60 w-screen h-screen z-40">
-                            <div className="relative w-[90%] max-w-md max-h-full">
-                                <div className="relative bg-white rounded-lg shadow">
-                                    <div className="p-6 text-center text-gray-600">
-                                        <div className="flex items-center justify-center">{QuestionIcon}</div>
-
-                                        <h3 className="mb-8 mt-4 text-lg font-normal">
-                                            Tem certeza que deseja finalizar este bolão?
-                                        </h3>
-
-                                        <div className="flex justify-center gap-4">
-                                            <button
-                                                type="button"
-                                                className="btn-secondary"
-                                                onClick={() => setModal(false)}
-                                            >
-                                                Cancelar
-                                            </button>
-                                            <button
-                                                type="button"
-                                                onClick={handleFinish}
-                                                className="btn-primary bg-red-700 hover:bg-red-800 focus:bg-red-800"
-                                            >
-                                                Finalizar
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )}
+                    {modal && <ModalDelete onConfirm={handleFinish} onClose={() => setModal(false)} />}
                 </div>
             ) : (
                 <div id="box-empty">Não existem dados para listar!</div>

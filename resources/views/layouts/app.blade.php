82bb567b5943d58a1a266706287e1806d1d77456
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2023-05-04 14:45:06 -0300

    Atualizando link do rodapé.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 
             <footer class="pb-8 text-center text-gray-500 text-xs">
                 <span>VinúBolão &copy; 2017-{{ now()->year }} - Criado por
-                    <a href="https://www.linkedin.com/in/gustavosantoslima" target="_blank" class="underline">
+                    <a href="https://www.gslima.dev" target="_blank" class="underline">
                         Gustavo Lima
                     </a>
                 </span>
